66be8cce4418432868dde431cc74587fd01d75e1
Arslan Siddique <108671491+as-Arslan-Siddique@users.noreply.github.com>
2024-08-09 11:52:26 +0500

    Update connector.py

--- mongodb/connector.py
+++ mongodb/connector.py
@@ -36,3 +36,30 @@ class MongoConnector:
         
         self.client.close()
         return {"status": 200, "message": "The data has been Successfully Deleted!"}
+    
+    def updateMany(self, data_model, input_data, query, query_var):
+        """
+            We will use this function to update data in MongoDB 
+            
+            "data_model": This variable will carry the model that we use to set the data to
+            "input_data": This variable will have the data that we want to limit before updation
+            "query": This is the filter to extract a chunk of data from the DB
+            "query_var": This is the variable that we use to find every row in the data from DB
+            
+        """
+
+        db = self.client["nt"]
+        collection = db["insights"]
+        
+        final_data = list(dict([dct["column"], row.get(dct["column"])] for dct in data_model) for row in input_data)
+        
+        for row in final_data:
+            query[query_var] = row[query_var]
+            newdata = {
+                "$set": row
+            }
+
+            collection.update_one(query, newdata)
+            
+        self.client.close()
+        return {"status": 200, "message": "The request was successfully processed!"}
